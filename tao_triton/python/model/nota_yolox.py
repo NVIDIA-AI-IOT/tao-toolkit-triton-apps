546465c478d687042020a2e3fae73355453aa05f
jeongmin <silverstar456@yonsei.ac.kr>
2022-04-02 05:22:04 +0000

    yolox 용 metrics 로깅 만들어 놓은것 추가

---
+++ tao_triton/python/model/nota_yolox.py
@@ -0,0 +1,254 @@
+import os
+from collections import defaultdict
+from webbrowser import get
+
+import numpy as np
+import cv2
+
+class YOLOX():
+    def __init__(self):
+        self.def_colors()
+        
+    # 전처리
+    def preprocess(self, img, input_size, swap=(2, 0, 1)):
+        if len(img.shape) == 3:
+            padded_img = np.ones((input_size[0], input_size[1], 3), dtype=np.uint8) * 114
+        else:
+            padded_img = np.ones(input_size, dtype=np.uint8) * 114
+
+        r = min(input_size[0] / img.shape[0], input_size[1] / img.shape[1])
+        resized_img = cv2.resize(
+            img,
+            (int(img.shape[1] * r), int(img.shape[0] * r)),
+            interpolation=cv2.INTER_LINEAR,
+        ).astype(np.uint8)
+        padded_img[: int(img.shape[0] * r), : int(img.shape[1] * r)] = resized_img
+
+        padded_img = padded_img.transpose(swap)
+        padded_img = np.ascontiguousarray(padded_img, dtype=np.float32)
+        return padded_img, r
+
+
+    # 후처리
+    def postprocess(self, inference_results, ratio, input_shape, nms_thr=0.45, score_thr=0.3):
+        predictions = self.demo_postprocess(inference_results, input_shape)[0]
+
+        boxes = predictions[:, :4]
+        scores = predictions[:, 4:5] * predictions[:, 5:]
+
+        boxes_xyxy = np.ones_like(boxes)
+        boxes_xyxy[:, 0] = boxes[:, 0] - boxes[:, 2]/2.
+        boxes_xyxy[:, 1] = boxes[:, 1] - boxes[:, 3]/2.
+        boxes_xyxy[:, 2] = boxes[:, 0] + boxes[:, 2]/2.
+        boxes_xyxy[:, 3] = boxes[:, 1] + boxes[:, 3]/2.
+        boxes_xyxy /= ratio
+
+        dets = self.multiclass_nms(boxes_xyxy, scores, nms_thr=nms_thr, score_thr=score_thr)
+        
+        return dets
+
+    def demo_postprocess(self, outputs, img_size, p6=False):
+
+        grids = []
+        expanded_strides = []
+
+        if not p6:
+            strides = [8, 16, 32]
+        else:
+            strides = [8, 16, 32, 64]
+
+        hsizes = [img_size[0] // stride for stride in strides]
+        wsizes = [img_size[1] // stride for stride in strides]
+
+        for hsize, wsize, stride in zip(hsizes, wsizes, strides):
+            xv, yv = np.meshgrid(np.arange(wsize), np.arange(hsize))
+            grid = np.stack((xv, yv), 2).reshape(1, -1, 2)
+            grids.append(grid)
+            shape = grid.shape[:2]
+            expanded_strides.append(np.full((*shape, 1), stride))
+
+        grids = np.concatenate(grids, 1)
+        expanded_strides = np.concatenate(expanded_strides, 1)
+        outputs[..., :2] = (outputs[..., :2] + grids) * expanded_strides
+        outputs[..., 2:4] = np.exp(outputs[..., 2:4]) * expanded_strides
+
+        return outputs
+
+    def multiclass_nms(self, boxes, scores, nms_thr, score_thr, class_agnostic=True):
+        """Multiclass NMS implemented in Numpy"""
+        if class_agnostic:
+            nms_method = self.multiclass_nms_class_agnostic
+        else:
+            nms_method = self.multiclass_nms_class_aware
+        return nms_method(boxes, scores, nms_thr, score_thr)
+
+
+    def multiclass_nms_class_agnostic(self, boxes, scores, nms_thr, score_thr):
+        """Multiclass NMS implemented in Numpy. Class-agnostic version."""
+        cls_inds = scores.argmax(1)
+        cls_scores = scores[np.arange(len(cls_inds)), cls_inds]
+
+        valid_score_mask = cls_scores > score_thr
+        if valid_score_mask.sum() == 0:
+            return None
+        valid_scores = cls_scores[valid_score_mask]
+        valid_boxes = boxes[valid_score_mask]
+        valid_cls_inds = cls_inds[valid_score_mask]
+        keep = self.nms(valid_boxes, valid_scores, nms_thr)
+        if keep:
+            dets = np.concatenate(
+                [valid_boxes[keep], valid_scores[keep, None], valid_cls_inds[keep, None]], 1
+            )
+        return dets
+
+
+    def nms(self, boxes, scores, nms_thr):
+        """Single class NMS implemented in Numpy."""
+        x1 = boxes[:, 0]
+        y1 = boxes[:, 1]
+        x2 = boxes[:, 2]
+        y2 = boxes[:, 3]
+
+        areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+        order = scores.argsort()[::-1]
+
+        keep = []
+        while order.size > 0:
+            i = order[0]
+            keep.append(i)
+            xx1 = np.maximum(x1[i], x1[order[1:]])
+            yy1 = np.maximum(y1[i], y1[order[1:]])
+            xx2 = np.minimum(x2[i], x2[order[1:]])
+            yy2 = np.minimum(y2[i], y2[order[1:]])
+
+            w = np.maximum(0.0, xx2 - xx1 + 1)
+            h = np.maximum(0.0, yy2 - yy1 + 1)
+            inter = w * h
+            ovr = inter / (areas[i] + areas[order[1:]] - inter)
+
+            inds = np.where(ovr <= nms_thr)[0]
+            order = order[inds + 1]
+
+        return keep
+
+
+    # 시각화
+    def vis(self, img, boxes, scores, cls_ids, conf=0.5, class_names=None):
+
+        for i in range(len(boxes)):
+            box = boxes[i]
+            cls_id = int(cls_ids[i])
+            score = scores[i]
+            if score < conf:
+                continue
+            x0 = int(box[0])
+            y0 = int(box[1])
+            x1 = int(box[2])
+            y1 = int(box[3])
+
+            color = (self._COLORS[cls_id] * 255).astype(np.uint8).tolist()
+            text = '{}:{:.1f}%'.format(class_names[cls_id], score * 100)
+            txt_color = (0, 0, 0) if np.mean(self._COLORS[cls_id]) > 0.5 else (255, 255, 255)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+
+            txt_size = cv2.getTextSize(text, font, 0.4, 1)[0]
+            cv2.rectangle(img, (x0, y0), (x1, y1), color, 2)
+
+            txt_bk_color = (self._COLORS[cls_id] * 255 * 0.7).astype(np.uint8).tolist()
+            cv2.rectangle(
+                img,
+                (x0, y0 + 1),
+                (x0 + txt_size[0] + 1, y0 + int(1.5*txt_size[1])),
+                txt_bk_color,
+                -1
+            )
+            cv2.putText(img, text, (x0, y0 + txt_size[1]), font, 0.4, txt_color, thickness=1)
+
+        return img
+
+
+    def def_colors(self):
+        self._COLORS = np.array(
+            [
+                0.000, 0.447, 0.741,
+                0.850, 0.325, 0.098,
+                0.929, 0.694, 0.125,
+                0.494, 0.184, 0.556,
+                0.466, 0.674, 0.188,
+                0.301, 0.745, 0.933,
+                0.635, 0.078, 0.184,
+                0.300, 0.300, 0.300,
+                0.600, 0.600, 0.600,
+                1.000, 0.000, 0.000,
+                1.000, 0.500, 0.000,
+                0.749, 0.749, 0.000,
+                0.000, 1.000, 0.000,
+                0.000, 0.000, 1.000,
+                0.667, 0.000, 1.000,
+                0.333, 0.333, 0.000,
+                0.333, 0.667, 0.000,
+                0.333, 1.000, 0.000,
+                0.667, 0.333, 0.000,
+                0.667, 0.667, 0.000,
+                0.667, 1.000, 0.000,
+                1.000, 0.333, 0.000,
+                1.000, 0.667, 0.000,
+                1.000, 1.000, 0.000,
+                0.000, 0.333, 0.500,
+                0.000, 0.667, 0.500,
+                0.000, 1.000, 0.500,
+                0.333, 0.000, 0.500,
+                0.333, 0.333, 0.500,
+                0.333, 0.667, 0.500,
+                0.333, 1.000, 0.500,
+                0.667, 0.000, 0.500,
+                0.667, 0.333, 0.500,
+                0.667, 0.667, 0.500,
+                0.667, 1.000, 0.500,
+                1.000, 0.000, 0.500,
+                1.000, 0.333, 0.500,
+                1.000, 0.667, 0.500,
+                1.000, 1.000, 0.500,
+                0.000, 0.333, 1.000,
+                0.000, 0.667, 1.000,
+                0.000, 1.000, 1.000,
+                0.333, 0.000, 1.000,
+                0.333, 0.333, 1.000,
+                0.333, 0.667, 1.000,
+                0.333, 1.000, 1.000,
+                0.667, 0.000, 1.000,
+                0.667, 0.333, 1.000,
+                0.667, 0.667, 1.000,
+                0.667, 1.000, 1.000,
+                1.000, 0.000, 1.000,
+                1.000, 0.333, 1.000,
+                1.000, 0.667, 1.000,
+                0.333, 0.000, 0.000,
+                0.500, 0.000, 0.000,
+                0.667, 0.000, 0.000,
+                0.833, 0.000, 0.000,
+                1.000, 0.000, 0.000,
+                0.000, 0.167, 0.000,
+                0.000, 0.333, 0.000,
+                0.000, 0.500, 0.000,
+                0.000, 0.667, 0.000,
+                0.000, 0.833, 0.000,
+                0.000, 1.000, 0.000,
+                0.000, 0.000, 0.167,
+                0.000, 0.000, 0.333,
+                0.000, 0.000, 0.500,
+                0.000, 0.000, 0.667,
+                0.000, 0.000, 0.833,
+                0.000, 0.000, 1.000,
+                0.000, 0.000, 0.000,
+                0.143, 0.143, 0.143,
+                0.286, 0.286, 0.286,
+                0.429, 0.429, 0.429,
+                0.571, 0.571, 0.571,
+                0.714, 0.714, 0.714,
+                0.857, 0.857, 0.857,
+                0.000, 0.447, 0.741,
+                0.314, 0.717, 0.741,
+                0.50, 0.5, 0
+            ]
+        ).astype(np.float32).reshape(-1, 3)
---
+++ tao_triton/python/utils/raw_metrics.py
@@ -0,0 +1,220 @@
+import cv2
+import os
+from tqdm import tqdm
+
+import numpy as np
+import torch
+
+from xml.etree.ElementTree import parse
+from collections import defaultdict
+
+from tao_triton.python.model.nota_yolox import YOLOX
+
+class RawMetrics:
+    def __init__(self):
+        self.yolox_data_dir = f'./datasets/VOCdevkit/VOC2012'
+        self.image_dir = f"{self.yolox_data_dir}/JPEGImages"
+        self.annotation_dir = f"{self.yolox_data_dir}/Annotations"
+        self.testset_txt_path = f"{self.yolox_data_dir}/ImageSets/Main/test.txt"
+        
+        self.npos = self._get_total_pos()
+        self.yolox = YOLOX()
+
+    def _get_actual_bboxes(self, xml_path):
+        tree = parse(xml_path)
+        root = tree.getroot()
+
+        objects = root.findall("object")
+        bboxes = [[0,0,0,0] for _ in range(len(objects))]
+
+        labels = []
+        for i, obj in enumerate(objects):
+            labels.append(obj.findtext("name"))
+            bboxes[i][0] = int(obj.find("bndbox").findtext("xmin"))
+            bboxes[i][1] = int(obj.find("bndbox").findtext("ymin"))
+            bboxes[i][2] = int(obj.find("bndbox").findtext("xmax"))
+            bboxes[i][3] = int(obj.find("bndbox").findtext("ymax"))
+        
+        return labels, bboxes
+    
+    
+    def _get_total_pos(self,):
+        anno_path = os.path.join(os.getcwd(), self.annotation_dir)
+        with open(self.testset_txt_path, "r", encoding="utf-8") as f:
+            files = f.readlines()
+            
+        total_post_class_dict = defaultdict(int)
+        for file in files:
+            file_path = os.path.join(anno_path, file[:-1]+".xml")
+            tree = parse(file_path)
+            root = tree.getroot()
+            objects = root.findall("object")
+            labels = [x.findtext("name") for x in objects]
+            for label in labels:
+                total_post_class_dict[label] += 1
+
+        return total_post_class_dict
+
+
+    def _count_res(self, pred_bboxes, pred_labels, actual_bboxes, actual_labels, iou_thr):
+        cnt = {
+            "car": np.array([0, 0]), 
+            "bus": np.array([0, 0]), 
+            "hdv": np.array([0, 0]), 
+            "truck": np.array([0, 0]), 
+            "motorcycle":np.array([0, 0]), 
+            "bicycle": np.array([0, 0]), 
+            "personal_mobility":np.array([0, 0]), 
+            "firetruck":np.array([0, 0]), 
+            "police":np.array([0, 0]), 
+            "ambulance":np.array([0, 0]), 
+            "pedestrian":np.array([0, 0])
+        }
+
+        if len(pred_labels) == 0 or len(actual_labels) == 0:
+            return cnt
+
+        for i, pred_bbox in enumerate(pred_bboxes):
+            for j, actual_bbox in enumerate(actual_bboxes):
+                iou = self._calc_iou(pred_bbox, actual_bbox)
+
+                if iou > iou_thr:
+                    if pred_labels[i] == actual_labels[j]:
+                        cnt[pred_labels[i]][1] += 1
+                    else:
+                        cnt[pred_labels[i]][0] += 1
+                else:
+                    continue
+
+        return cnt
+
+
+    def _calc_iou(self, box1, box2):
+        # box = (x1, y1, x2, y2)
+        box1_area = (box1[2] - box1[0] + 1) * (box1[3] - box1[1] + 1)
+        box2_area = (box2[2] - box2[0] + 1) * (box2[3] - box2[1] + 1)
+
+        # obtain x1, y1, x2, y2 of the intersection
+        x1 = max(box1[0], box2[0])
+        y1 = max(box1[1], box2[1])
+        x2 = min(box1[2], box2[2])
+        y2 = min(box1[3], box2[3])
+
+        # compute the width and height of the intersection
+        w = max(0, x2 - x1 + 1)
+        h = max(0, y2 - y1 + 1)
+
+        inter = w * h
+        iou = inter / (box1_area + box2_area - inter)
+        return iou
+
+
+    def predict(self, img_path, model, score_thr, nms_thr):
+        origin_img = cv2.imread(img_path)
+        # print(origin_img.shape)
+
+        input_shape = "544,960"
+        input_shape = tuple(map(int, input_shape.split(',')))
+        img, ratio = self.yolox.preprocess(origin_img, input_shape)
+    
+        model = model.eval()        
+        output = model(torch.tensor(img).view(1, 3, input_shape[0], input_shape[1]).cuda()).detach()
+        output = torch.cat([output[..., :4], output[..., 4:].sigmoid()], dim=-1)
+        output = output.cpu().numpy()
+
+        predictions = self.yolox.demo_postprocess(output, input_shape)[0]
+
+        boxes = predictions[:, :4]
+        scores = predictions[:, 4:5] * predictions[:, 5:]
+        
+        boxes_xyxy = np.ones_like(boxes)
+        boxes_xyxy[:, 0] = boxes[:, 0] - boxes[:, 2]/2.
+        boxes_xyxy[:, 1] = boxes[:, 1] - boxes[:, 3]/2.
+        boxes_xyxy[:, 2] = boxes[:, 0] + boxes[:, 2]/2.
+        boxes_xyxy[:, 3] = boxes[:, 1] + boxes[:, 3]/2.
+        boxes_xyxy /= ratio
+
+        dets = self.yolox.multiclass_nms(boxes_xyxy, scores, nms_thr=nms_thr, score_thr=score_thr)
+
+        if dets is None:
+            return None
+        else:
+            final_boxes, final_scores, final_cls_inds = dets[:, :4], dets[:, 4], dets[:, 5]
+
+        label_names = ["car", "bus", "hdv", "truck", "motorcycle", "bicycle", "personal_mobility", "firetruck", "police", "ambulance", "pedestrian"]
+
+        results = []
+        for i in range(dets.shape[0]):
+            results.append({
+                "label": label_names[int(final_cls_inds[i])],
+                "points": list(final_boxes[i]),
+            })
+
+        return results
+
+
+    def get_raw_metrics(
+        self, 
+        model,
+        nms_thr=0.45, 
+        score_thr=0.4, 
+        iou_thr=0.5
+    ):
+        with open(self.testset_txt_path) as f:
+            lines = f.read().splitlines()
+
+
+        cnts = {
+            "car": np.array([0, 0]), 
+            "bus": np.array([0, 0]), 
+            "hdv": np.array([0, 0]), 
+            "truck": np.array([0, 0]), 
+            "motorcycle":np.array([0, 0]), 
+            "bicycle": np.array([0, 0]), 
+            "personal_mobility":np.array([0, 0]), 
+            "firetruck":np.array([0, 0]), 
+            "police":np.array([0, 0]), 
+            "ambulance":np.array([0, 0]), 
+            "pedestrian":np.array([0, 0])
+        }
+
+        for line in tqdm(lines):
+            results = self.predict(
+                img_path=f"{self.image_dir}/{line}.jpg", 
+                model=model, 
+                score_thr=score_thr, 
+                nms_thr=nms_thr
+            )
+            if results is None:
+                continue
+
+            result = {"point": [], "label": []}
+            for r in results:
+                result["point"].append(r["points"])
+                result["label"].append(r["label"])
+                
+            actual_labels, actual_bboxes = self._get_actual_bboxes(xml_path=f"{self.annotation_dir}/{line}.xml")
+            
+            cnt = self._count_res(
+                pred_bboxes=result["point"], 
+                pred_labels=result["label"], 
+                actual_bboxes=actual_bboxes, 
+                actual_labels=actual_labels,
+                iou_thr=iou_thr,
+            )
+
+            for cls in cnt.keys():
+                cnts[cls] += cnt[cls]
+        
+        metrics = {}
+        for cls in cnt.keys():
+            fp = cnts[cls][0]
+            tp = cnts[cls][1]
+            fn = self.npos[cls] - tp
+            
+            metrics[f"{cls}_FP"] = fp
+            metrics[f"{cls}_TP"] = tp
+            metrics[f"{cls}_FN"] = fn
+            
+        return metrics
+    
