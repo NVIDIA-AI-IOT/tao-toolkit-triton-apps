fd58705f16e25c3daaa2f854b1490ce397705357
Varun Praveen <vpraveen@nvidia.com>
2023-09-21 18:40:21 +0000

    [ChageFormer] Enable changenet segmentation

--- scripts/config.sh
+++ scripts/config.sh
@@ -43,6 +43,7 @@ tlt_key_re_identification="nvidia_tao"
 peoplenet_version="pruned_quantized_decrypted_v2.3.3"
 dashcamnet_version="pruned_v1.0.4"
 vehicletypenet_version="pruned_v1.0.1"
+visual_changenet_version="changenet_levircd_deployable_v1.0"
 
 # Setting model version to run inference on for Pose Classification.
 pc_peoplenet_version="nvidia/tao/peoplenet:deployable_quantized_v2.5"
@@ -59,5 +60,6 @@ ngc_retinanet="https://nvidia.box.com/shared/static/3a00fdf8e1s2k3nezoxmfyykydxi
 ngc_mcls_classification="https://docs.google.com/uc?export=download&id=1blJQDQSlLPU6zX3yRmXODRwkcss6B3a3"
 ngc_pose_classification="https://api.ngc.nvidia.com/v2/models/nvidia/tao/poseclassificationnet/versions/deployable_v1.0/zip"
 ngc_re_identification="https://drive.google.com/uc?export=download&id=1jicWzrPgEgvHLoxS57XLwk3o2xRbXeN_"
+ngc_visual_changenet="nvstaging/tao/changenet_segmentation:${visual_changenet_version}"
 
 default_model_download_path="${tao_triton_root}/tao_models"
--- tao_triton/python/postprocessing/visual_changenet_postprocessor.py
+++ tao_triton/python/postprocessing/visual_changenet_postprocessor.py
@@ -32,62 +32,67 @@ from tao_triton.python.postprocessing.postprocessor import Postprocessor
 from tao_triton.python.postprocessing.utils import pool_context
 
 
-    
-def make_numpy_grid(tensor_data, pad_value=0,padding=0,num_class=2,gt=None):
+def make_numpy_grid(tensor_data, pad_value=0, padding=0, num_class=2, gt=None):
+    """Make a numpy grid."""
     vis = tensor_data.detach()
     vis = utils.make_grid(tensor_data, pad_value=0,padding=0)
     vis = np.array(vis.cpu()).transpose((1,2,0))
 
-    if num_class>2:
-        #multi-class visualisation
-        #TODO: Supports 10 classes - make it more flexible and not hard coded
+    if num_class > 2:
+        # multi-class visualisation
+        # FIXME: Supports 10 classes - make it more flexible and not hard coded
         vis_multi = vis[:,:,0]
 
         #Code for visualising FN/FP (gt!=pred)
         if isinstance(gt, torch.Tensor):
             gt = gt.detach()
-            gt = utils.make_grid(gt, pad_value=pad_value,padding=padding)
+            gt = utils.make_grid(
+                gt,
+                pad_value=pad_value,
+                padding=padding
+            )
             gt = np.array(gt.cpu()).transpose((1,2,0))[:,:,0]
 
     if vis.shape[2] == 1:
         vis = np.stack([vis, vis, vis], axis=-1)
 
-    
     if num_class>2:
         color_coded = np.ones(np.shape(vis))
         for i in range(10):
             color_coded[vis_multi==i] = colour_mappings[str(i)]
-        color_coded= color_coded/255
+        color_coded = color_coded / 255
         color_coded  = color_coded.astype(float)
-        
         #Code for visualising FN/FP (gt!=pred)
         if isinstance(gt, np.ndarray):
             color_coded_mismatch = np.copy(color_coded)
             color_coded_mismatch[vis_multi!=gt] = (0,0,0)
             color_coded_mismatch  = color_coded_mismatch.astype(float)
             return color_coded, color_coded_mismatch
-
         return color_coded
     else: 
         return vis
 
+
 def _visualize_pred(pred):
-    import torch
+    """Return visualized predictions."""
     pred = torch.Tensor(pred)
     pred = torch.argmax(pred, dim=1, keepdims=True)
     pred_vis = pred * 255
     return pred_vis
 
+
 def de_norm(tensor_data):
+    """Denormalize a torch tensor."""
     tensor_data = tensor_data * 0.5 + 0.5
     tensor_data = torch.Tensor(tensor_data)
     return tensor_data
 
-class ChangeFormerPostprocessor(Postprocessor):
+
+class VisualChangeNetPostprocessor(Postprocessor):
     """Class to run post processing of Triton Tensors."""
 
     def __init__(self, batch_size, frames, output_path, data_format):
-        """Initialize a post processor class for a changeformer model.
+        """Initialize a post processor class for a visual_changenet model.
         
         Args:
             batch_size (int): Number of images in the batch.
@@ -98,20 +103,21 @@ class ChangeFormerPostprocessor(Postprocessor):
                 "channels_last": HWC order.
         """
         super().__init__(batch_size, frames, output_path, data_format)
-        self.output_names = ["output",
-                             "6746",
-                             "6833",
-                             "6920",
-                             "6938"]
-        self.final_output = "6938"
-        
-    def apply(self, results, this_id, render=True, batching=True, img_name=None):
-        """Apply the post processor to the outputs to the changeformer outputs."""
+        self.output_names = [
+            "output0",
+            "output1",
+            'output2',
+            'output3',
+            'output_final'
+        ]
+        self.final_output = "output_final"
 
-        output_cf = results.as_numpy(self.final_output)
-
+    def apply(self, output_tensors, this_id, render=True, batching=True, img_name=None):
+        """Apply the post processor to the outputs to the visual_changenet outputs."""
+        output_cf = output_tensors.as_numpy(self.final_output)
         vis = make_numpy_grid(_visualize_pred(output_cf))
         vis = np.clip(vis, a_min=0.0, a_max=1.0)
         file_name = os.path.join(
-            self.output_path, 'eval_' + str(img_name)+'.jpg')
+            self.output_path, f'eval_{str(img_name)}.jpg'
+        )
         plt.imsave(file_name, vis)
